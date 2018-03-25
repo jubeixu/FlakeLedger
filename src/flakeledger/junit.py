78601374ffb57a53138c0ec70552e1a6d54a9f75
jubeixu <12108555+jubeixu@users.noreply.github.com>
2018-03-25 17:29:00 +0700

    test: cover junit xml

---
+++ src/flakeledger/junit.py
@@ -0,0 +1,19 @@
+"""Parse JUnit XML result files into structured test case records.
+
+The parser uses xml.etree from the standard library. It reads the common
+JUnit schema produced by pytest, Gradle, Maven Surefire, and similar tools:
+a `testsuites` root (or a single `testsuite`) containing `testcase` elements.
+
+Each `testcase` may carry a child element that marks its outcome:
+  - `<failure>` or `<error>`  the test did not pass
+  - `<skipped>`               the test was skipped
+  - no child element          the test passed
+
+We also read metadata that identifies which CI run a suite belongs to.
+Two attributes drive the rest of the pipeline:
+  - `commit`   the source revision under test
+  - `attempt`  the rerun index for that commit (1 for the first run)
+
+These are read from the `testsuite` (or `testsuites`) element. They are not
+part of the base JUnit schema, so a producer must set them as attributes or
+as `<property>` entries. When they are absent we fall back to documented
