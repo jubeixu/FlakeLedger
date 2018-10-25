662276e235c6b28939cf69842fb851a1a9250428
jubeixu <12108555+jubeixu@users.noreply.github.com>
2018-10-25 10:12:00 +0700

    perf: improve ci cost attribution

--- src/flakeledger/report.py
+++ src/flakeledger/report.py
@@ -7,3 +7,12 @@ cleanly in git. Currency figures are rounded to a fixed number of places.
 from __future__ import annotations
 
 from flakeledger.classify import Classification
+from flakeledger.cost import Rates, TestCost
+from flakeledger.junit import CaseResult
+from flakeledger.runs import TestOnCommit
+
+
+def render_ingest(results: list[CaseResult], warnings: list[str]) -> str:
+    lines: list[str] = []
+    lines.append(f"cases {len(results)}")
+    commits = sorted({r.commit for r in results})
