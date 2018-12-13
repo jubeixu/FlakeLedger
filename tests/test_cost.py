520399e92d664143fed841d69f274254e5724692
jubeixu <12108555+jubeixu@users.noreply.github.com>
2018-12-13 11:30:00 +0700

    fix: compute cost

--- tests/test_cost.py
+++ tests/test_cost.py
@@ -8,3 +8,13 @@ from flakeledger.cost import Rates, cost_for_flaky_tests
 from flakeledger.junit import parse_paths
 from flakeledger.runs import group_by_test_commit
 from flakeledger import cli
+
+SAMPLES = Path(__file__).resolve().parents[1] / "samples"
+
+
+def _records():
+    files = sorted(str(p) for p in SAMPLES.glob("*.xml"))
+    results, _ = parse_paths(files)
+    return group_by_test_commit(results)
+
+
