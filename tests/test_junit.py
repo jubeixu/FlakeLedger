c82c9d11319353e01326dfbf3b8ee8566da7cdc3
jubeixu <12108555+jubeixu@users.noreply.github.com>
2019-01-07 15:47:00 +0700

    fix: correct cli report

--- tests/test_junit.py
+++ tests/test_junit.py
@@ -3,3 +3,9 @@ from pathlib import Path
 
 from flakeledger.junit import FAILED, PASSED, parse_file, parse_paths
 
+SAMPLES = Path(__file__).resolve().parents[1] / "samples"
+
+
+class TestJunitParsing(unittest.TestCase):
+    def test_parses_attribute_metadata(self):
+        results, warnings = parse_file(SAMPLES / "run-a1b2c3-attempt1.xml")
