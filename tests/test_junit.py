93c64887c82beb2632da0a601c59d45b75350522
jubeixu <12108555+jubeixu@users.noreply.github.com>
2019-01-07 15:50:00 +0700

    style: clean up deterministic render

--- tests/test_junit.py
+++ tests/test_junit.py
@@ -9,3 +9,10 @@ SAMPLES = Path(__file__).resolve().parents[1] / "samples"
 class TestJunitParsing(unittest.TestCase):
     def test_parses_attribute_metadata(self):
         results, warnings = parse_file(SAMPLES / "run-a1b2c3-attempt1.xml")
+        self.assertEqual(warnings, [])
+        self.assertEqual(len(results), 4)
+        commits = {r.commit for r in results}
+        self.assertEqual(commits, {"a1b2c3"})
+        self.assertTrue(all(r.attempt == 1 for r in results))
+
+    def test_status_detection(self):
